afa2a002c0ffbe87999db2a73fc9d62f1c6a7018
javVM <jverdemarin@gmail.com>
2023-01-23 21:17:15 +0100

    Update server

--- servers/routes/routes.js
+++ servers/routes/routes.js
@@ -1,3 +1,5 @@
 const router = require('express').Router();
-
+const middlewares = require('../middlewares/middlewares');
+router.route('/balance').get(middlewares.getBalance);
+router.route('/download-balance').get(middlewares.downloadBalance);
 module.exports = router;
--- servers/server.js
+++ servers/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.NODE_PORT;
+const port = process.env.NODE_PORT || "3000";
 
 app.listen(port, () => {
     console.log(`Successfully listening on port ${port}`);
